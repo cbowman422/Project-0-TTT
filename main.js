1ede50c034a2ba82c357925738eb7ce0edae3002
cbowman422 <chrisb0wman@hotmail.com>
2022-11-10 14:02:11 -0500

    got my first eventListener to work with button 1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("welcome")
+
+
+
+buttonOne = document.querySelector('#button1')
+buttonOne.addEventListener('click', buttonOneClick)
+
+function buttonOneClick(){
+    console.log("button 1 clicked")
+}
